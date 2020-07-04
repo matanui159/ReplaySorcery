ef480539da2160b1fbcc8aa721ea296db50f8c72
Joshua Minter <josh@minteronline.com>
2020-07-04 14:26:34 +1000

    License code under MPL

--- src/encoder/encoder.h
+++ src/encoder/encoder.h
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 #ifndef SR_ENCODER_H
 #define SR_ENCODER_H
 #include "../pktcircle.h"
--- src/pktcircle.h
+++ src/pktcircle.h
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 #ifndef SR_PKTCIRCLE_H
 #define SR_PKTCIRCLE_H
 #include <pthread.h>
