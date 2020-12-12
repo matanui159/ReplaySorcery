075dfb225beb5885423c071a05bee8123209b3dd
Joshua Minter <josh@minteronline.com>
2020-12-12 18:43:44 +1000

    Run clang-format

--- src/main.c
+++ src/main.c
@@ -49,7 +49,9 @@ static int mainStep(void) {
    int ret;
    while ((ret = rsEncoderNextPacket(&videoEncoder, &videoPacket)) == AVERROR(EAGAIN)) {
       if ((ret = rsDeviceNextFrame(&videoDevice, videoFrame)) < 0) {
-         av_log(NULL, AV_LOG_WARNING, "Failed to get packet from device (%s), silencing logs for now\n", av_err2str(ret));
+         av_log(NULL, AV_LOG_WARNING,
+                "Failed to get packet from device (%s), silencing logs for now\n",
+                av_err2str(ret));
          av_log_set_level(AV_LOG_FATAL);
       } else {
          av_log_set_level(rsConfig.logLevel);
